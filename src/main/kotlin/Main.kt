eb12edef4d03472724ca2221e631c70cdec4291b
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-10-06 06:03:34 +0530

    kotlin loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,29 @@ import java.util.*
     fun main(args: Array<String>) {
         val sc = Scanner(System.`in`)
         var a = sc.nextInt()
+
+        /*when (a) {
+            1 -> print("x == 1")
+            2 -> print("x == 2")
+            else -> {
+                print("x is neither 1 nor 2")
+            }
+        }*/
+
+        for (i in 1..3) {
+            println(i)
+        }
+        /*for (i in 6 downTo 0 step 2) {
+            println(i)
+        }*/
+
         pri(a)
 
     }
 
-    fun pri(a:Int) {
+
+
+fun pri(a:Int) {
         if (a == 0) {
             return
         }
